6b66253767bae45c5716eb52ea49ed3c61f32dee
shyamn_17 <shyamn2001@gmail.com>
2025-01-27 17:43:10 +0530

    menu updated

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -4,21 +4,15 @@ import Shimmer from "./Shimmer";
 import useRestaurant from "../Hooks/useRestaurant";
 import { addItem } from "../Redux/Slices/cartSlice";
 import { useDispatch } from "react-redux";
+import veg from "../Assets/veg.jpg";  
+import nonveg from "../Assets/nonveg.jpg";  
 
 const getVegSymbol = (vegClassifier) => {
-  const commonStyles = "w-6 h-6";
+  const commonStyles = "w-5 h-5"; 
   if (vegClassifier === "VEG") {
-    return (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={`${commonStyles} text-green-500`}>
-        <path d="M9 12l2 2 4-4M9 12h2m2 0h2" />
-      </svg>
-    );
+    return ( <img src={veg} alt="Veg" className={`${commonStyles}`} /> );
   } else {
-    return (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={`${commonStyles} text-red-500`}>
-        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm1-13h-2v6h2zm0 8h-2v2h2z" />
-      </svg>
-    );
+    return (<img src={nonveg} alt="Non-Veg" className={`${commonStyles}`} />);
   }
 };
 
@@ -37,6 +31,7 @@ const RestaurantMenu = () => {
 
   const restaurantInfo = restaurant?.cards?.[2]?.card?.card?.info;
   const menuItems = restaurant?.cards?.[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards;
+  console.log(menuItems);
 
   return (
     <div className="bg-[#F7F9FB] min-h-screen py-10 flex flex-col items-center">
@@ -85,23 +80,26 @@ const RestaurantMenu = () => {
                     </div>
                     <h3 className="text-xl font-semibold text-[#000000]">{itemCards?.card?.info?.name}</h3>
                         <p className="text-lg font-semibold text-[#603F83FF]">
-                    ₹ {itemCards?.card?.info?.price / 100}
+                    ₹ {itemCards?.card?.info?.defaultPrice / 100}
                   </p>
-                  <p className="text-sm text-gray-600 mt-2">{itemCards?.card?.info?.description}</p>
+                  <p className="text-sm text-gray-600 mt-2 mr-4">{itemCards?.card?.info?.description}</p>
                 </div>
                 <div className="flex flex-col items-center">
-                  <img
-                    src={IMG_CDN + itemCards?.card?.info?.imageId}
-                    alt={itemCards?.card?.info?.name}
-                    className="w-20 h-20  rounded-md ml-1 "
-                  />
-                  <button
-                    className="mt-4 bg-[#603F83FF] text-white text-sm px-4 py-2 rounded-lg hover:bg-[#503F73] transition duration-300"
-                    onClick={() => addFoodItem(itemCards)}
-                  >
-                    Add
-                  </button>
-                </div>
+  <div className="w-32 h-32 relative">
+    <img
+      src={IMG_CDN + itemCards?.card?.info?.imageId}
+      alt={itemCards?.card?.info?.name}
+      className="rounded-t-md object-cover w-full h-full"
+    />
+  </div>
+  <button
+    className="bg-[#603F83FF] text-white text-sm px-4 py-2 w-32 rounded-b-md hover:bg-[#503F73] transition duration-300 -mt-6 translate-y-1/4"
+    onClick={() => addFoodItem(itemCards)}
+  >
+    Add
+  </button>
+</div>
+
               </li>
             ))
           ) : (

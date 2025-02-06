dfa71df43bf8f5edcdb4184beeb3ba1a2c05bda6
shyamn17 <shyamn2001@gmail.com>
2025-02-07 00:40:52 +0530

    cors

--- src/Services/Endpoints.jsx
+++ src/Services/Endpoints.jsx
@@ -1,7 +1,7 @@
-export const IMG_CDN="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const IMG_CDN="https://cors-anywhere.herokuapp.com/https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const RESTAURANT_MENU="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550&restaurantId=";
+export const RESTAURANT_MENU="https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.96340&lng=77.58550&restaurantId=";
 
-export const RESTAURANTS="https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const RESTAURANTS="https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
-export const INSTAMART="https://www.swiggy.com/api/instamart/home?pageNo=1&layoutId=2073&storeId=911032&primaryStoreId=911032&secondaryStoreId=&clientId=INSTAMART-APP";
+export const INSTAMART="https://cors-anywhere.herokuapp.com/https://www.swiggy.com/api/instamart/home?pageNo=1&layoutId=2073&storeId=911032&primaryStoreId=911032&secondaryStoreId=&clientId=INSTAMART-APP";
